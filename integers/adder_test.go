15f6b067bc161768023f37fa2aa102e16e763843
Carlos Rodrigo <hi@carlosrodrigo.me>
2019-08-25 20:31:19 -0300

    refactor extracting duplicated assert into a function

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -4,21 +4,23 @@ import "testing"
 
 func TestAdder(t *testing.T) {
 
+	assertAdderSumResult := func(t *testing.T, sum, expect int) {
+		t.Helper()
+		if sum != expect {
+			t.Errorf("expected %d but got %d", expect, sum)
+		}
+	}
 	t.Run("sum 2 plus 2 and expect 4 as result", func(t *testing.T) {
 		sum := Add(2, 2)
 		expect := 4
 
-		if sum != expect {
-			t.Errorf("expected %d but got %d", expect, sum)
-		}
+		assertAdderSumResult(t, sum, expect)
 	})
 
 	t.Run("sum 2 plus 3 and expect 5 as result", func(t *testing.T) {
 		sum := Add(2, 3)
 		expect := 5
 
-		if sum != expect {
-			t.Errorf("expected %d but got %d", expect, sum)
-		}
+		assertAdderSumResult(t, sum, expect)
 	})
 }
