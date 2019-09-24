bd0c3d8d0754e1ff367e89910970efa41a665284
carlos-rodrigo <hi@carlosrodrigo.me>
2019-09-24 07:25:07 -0300

    Arrays

--- arrays-and-slices/arrays_test.go
+++ arrays-and-slices/arrays_test.go
@@ -7,6 +7,13 @@ import (
 
 func TestSum(t *testing.T) {
 
+	assertSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 
@@ -22,17 +29,20 @@ func TestSum(t *testing.T) {
 		got := SumAll([]int{1, 2}, []int{0, 9})
 		want := []int{3, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		assertSums(t, got, want)
 	})
 
 	t.Run("calculate the totals of the tails of each slice", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		assertSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		assertSums(t, got, want)
 	})
 }
