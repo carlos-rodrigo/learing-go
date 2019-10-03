96e89f79bb27c138fed5d3c42b472280f03b74a9
Carlos Rodrigo <csrc08@gmail.com>
2019-10-03 07:23:48 -0300

    dependency injection chapter init

---
+++ dependency_injection/di_test.go
@@ -0,0 +1,18 @@
+package dependency_injection
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Carlos")
+
+	got := buffer.String()
+	want := "Hello, Carlos"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
